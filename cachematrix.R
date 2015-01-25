18b667d1d05d137de13f5bf59f099430debc4f5c
Steven88 <stonesok@aol.com>
2015-01-25 13:06:57 -0600

    new matrix code

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x = matrix()) {    ##function to create matrix
+  ms <- NULL                   ## make solved matrix = NULL
+  set <- function(y) {    ##functon to create the cached data
+    x <<- y                 ## assign y to x and caches in parent envrnmt
+    ms <<- NULL         ## assign null to ms and caches in parent envrnmt
+  }
+  get <- function() x                 ## get the x matrix
+  setsolve <- function(solve) ms <<- solve   ## calculate inverted matrix
+  getsolve <- function() ms          ## call function for inverted matrix
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)                          ## make List for functions
+}
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+Cachesolve <- function(x, ...) {      ## assigns above matrix function to cachesolve
+  ms <- x$getsolve()               ## assign solve to ms, from parent environment
+  if(!is.null(ms)) {                   ## Decide if ms is not null
+    message("getting cached data")   ## if ms is not null, display message
+    return(ms)                           ## returns inverse matrix 
+  }
+  
+  data <- x$get()                     ## get x from parent envrnmt, assign to data
+  ms <- solve(data, ...)           ## performs solve on data, to inverse matrix
+  x$setsolve(ms)                   ## saves solved data/matrix in parent envirnmt
+  ms                                        ## displays inverse matrix
 }
 
 
